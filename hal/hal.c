875c00400968b821a50dbc70bb0838a9cfdb4e87
Michael Reutman <mreutman@gmail.com>
2019-01-23 21:07:12 -0800

    Add test case for Piezo.

--- hal/hal.c
+++ hal/hal.c
@@ -4,8 +4,13 @@
 #include "bme680.h"
 #include "icm20602.h"
 #include "driver/i2c.h"
+#include "driver/ledc.h"
 
 /***** Defines *****/
+
+#define _PWM_PIEZO_PIN 18
+#define _PWM_RESOLUTION (LEDC_TIMER_16_BIT)
+#define _PWM_FREQ_HZ 1000
 
 #define _I2C_SDA_PIN 32
 #define _I2C_SCL_PIN 33
@@ -20,6 +25,9 @@ static struct bme680_dev _bme680;
 static struct icm20602_dev _icm20602;
 static i2c_port_t _i2c = I2C_NUM_0;
 static SemaphoreHandle_t _lock = NULL;
+
+static const ledc_mode_t _speed_mode = LEDC_HIGH_SPEED_MODE;
+static const ledc_channel_t _channel = LEDC_CHANNEL_0;
 
 /***** Local Functions *****/
 
@@ -190,6 +198,78 @@ _icm20602_init(void)
   }
 
   return r;
+}
+
+static bool
+_piezo_init(void)
+{
+  ledc_channel_config_t ledc_conf;
+  ledc_timer_config_t timer_conf;
+  esp_err_t err = ESP_OK;
+  bool r = true;
+
+  if (r) {
+    timer_conf.duty_resolution = _PWM_RESOLUTION;
+    timer_conf.freq_hz = _PWM_FREQ_HZ;
+    timer_conf.speed_mode = _speed_mode;
+    timer_conf.timer_num = LEDC_TIMER_0;
+    err = ledc_timer_config(&timer_conf);
+    if (ESP_OK != err) {
+      r = false;
+    }
+  }
+
+  if (r) {
+    ledc_conf.timer_sel = LEDC_TIMER_0;
+    ledc_conf.channel = _channel;
+    ledc_conf.duty = 0;
+    ledc_conf.gpio_num = _PWM_PIEZO_PIN;
+    ledc_conf.intr_type = LEDC_INTR_DISABLE;
+    ledc_conf.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_conf.hpoint = UINT16_MAX - 1;
+    err = ledc_channel_config(&ledc_conf);
+    if (ESP_OK != err) {
+      r = false;
+    }
+  }
+
+  return r;
+}
+
+static bool
+_piezo_on(void)
+{
+  esp_err_t err = ESP_OK;
+
+  if (ESP_OK == err) {
+    err = ledc_set_duty(_speed_mode, _channel, 200);
+  }
+
+  if (ESP_OK == err) {
+    err = ledc_update_duty(_speed_mode, _channel);
+  }
+
+  return (ESP_OK == err) ? true : false;
+}
+
+static bool
+_piezo_off(void)
+{
+  esp_err_t err = ESP_OK;
+
+  if (ESP_OK == err) {
+    err = ledc_set_duty(_speed_mode, _channel, 0);
+  }
+
+  if (ESP_OK == err) {
+    err = ledc_update_duty(_speed_mode, _channel);
+  }
+
+  if (ESP_OK == err) {
+    err = ledc_stop(_speed_mode, _channel, 0);
+  }
+
+  return (ESP_OK == err) ? true : false;
 }
 
 /***** Global Functions *****/
@@ -365,6 +445,25 @@ TEST_CASE("HAL ICM20602", "[hal.c]")
 
   printf("* Free I2C interface.\n");
   r = _i2c_master_free();
+  TEST_ASSERT(r);
+}
+
+TEST_CASE("HAL piezo", "[hal.c]")
+{
+  bool r = true;
+
+  printf("* Initializing PWM interface.\n");
+  r = _piezo_init();
+  TEST_ASSERT(r);
+
+  printf("* Turning on piezo.\n");
+  r = _piezo_on();
+  TEST_ASSERT(r);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  printf("* Turning off piezo.\n");
+  r = _piezo_off();
   TEST_ASSERT(r);
 }
 #endif
